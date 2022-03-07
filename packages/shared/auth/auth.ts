da5fd17b071907297d294b4ee03b77a6b2985d6c
Ali Zahid <ali.zahid@live.com>
2022-03-07 11:50:29 +0100

    Refactoring

--- packages/shared/auth/auth.ts
+++ packages/shared/auth/auth.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosRequestHeaders } from 'axios'
 
 import { NextPageRequest } from '../types/next'
 import { Profile } from '../types/profile'
@@ -6,13 +6,17 @@ import { Profile } from '../types/profile'
 export const fetchProfile = async (
   req?: NextPageRequest
 ): Promise<Profile | null> => {
+  const headers: AxiosRequestHeaders = {}
+
+  if (req?.cookies.token) {
+    headers.authorization = req.cookies.token
+  }
+
   const {
     data: { user }
   } = await axios.get('/api/auth/profile', {
     baseURL: process.env.NEXT_PUBLIC_URL_SOUR,
-    headers: {
-      authorization: req?.cookies.token
-    },
+    headers,
     withCredentials: true
   })
 
